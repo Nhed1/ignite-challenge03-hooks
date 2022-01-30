52adc54221d8096916645e1aa6c385020432e1a9
Leonardo Garreto <leonardogarreto@gmail.com>
2022-01-30 14:29:44 -0300

    initial commit reseted

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { MdShoppingBasket } from "react-icons/md";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
-import logo from "../../assets/images/logo.svg";
-import { Container, Cart } from "./styles";
-import { useCart } from "../../hooks/useCart";
+import logo from '../../assets/images/logo.svg';
+import { Container, Cart } from './styles';
+import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  const { cart } = useCart();
-  const uniqueArray = cart.filter((item, index, array) => {
-    return [array.indexOf(item) === index].length;
-  });
-  const cartSize = uniqueArray.length;
+  // const { cart } = useCart();
+  // const cartSize = // TODO;
 
   return (
     <Container>
@@ -23,7 +20,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
